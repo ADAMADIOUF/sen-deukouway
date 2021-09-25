e66a828c3ea94767d88ef23d83c7c476a41e95d9
ADAMADIOUF <adamadiouf2017@gmail.com>
2021-09-25 11:13:10 -0500

    done

--- js/slider.js
+++ js/slider.js
@@ -42,10 +42,3 @@ function carousel() {
 
 prevBtn.style.display = 'none'
 
-
-// Aos
-AOS.init({
-  offset: 400, // offset (in px) from the original trigger point
-  delay: 0, // values from 0 to 3000, with step 50ms
-  duration: 1000, // values from 0 to 3000, with step 50ms
-})
